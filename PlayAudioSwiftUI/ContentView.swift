14f8a84142fe4f04111df727fb2e21ed7ddad6fd
Sandy Gomez <sgomez1029@gmail.com>
2024-05-16 06:53:52 -0700

    Initial Commit

---
+++ PlayAudioSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayAudioSwiftUI
+//
+//  Created by Sandra Gomez on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayAudioSwiftUI/PlayAudioSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayAudioSwiftUIApp.swift
+//  PlayAudioSwiftUI
+//
+//  Created by Sandra Gomez on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayAudioSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
